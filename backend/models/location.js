843d99c1f1144db444eb3ca7f457629872c56a42
Anna <annasaltveit@gmail.com>
2025-01-14 20:36:26 -0800

    backend-add location model

---
+++ backend/models/location.js
@@ -0,0 +1,70 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config/database');
+const Location = db.define('location', {
+    uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdYearStart: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        min: 0,
+        max: 2030,
+    },
+    createdYearEnd: {
+        type: DataTypes.INTEGER,
+        min: 0,
+        max: 2030,
+        isAfterCreatedYearStart(value) {
+            if (value <= this.createdYearStart) {
+              throw new Error('createdYearEnd must be after createdYearStart.');
+            }
+        }
+    },
+    discoveredYear: {
+        type: DataTypes.INTEGER,
+        min: 0,
+        max: 2030,
+    },
+    longitude: {
+        type: DataTypes.REAL,
+        defaultValue: 0,
+        min: -180,
+        max: 180,
+        allowNull: false,
+    },
+    latitude: {
+        type: DataTypes.REAL,
+        defaultValue: 0,
+        min: -90,
+        max: 90,
+        allowNull: false,
+    },
+    text: {
+        type: DataTypes.STRING,
+    },
+    place: {
+        type: DataTypes.STRING,
+    },
+    location: {
+        type: DataTypes.STRING,
+    },
+    script: {
+        type: DataTypes.STRING,
+    },
+    text: {
+        type: DataTypes.STRING,
+    },
+    shelfmark: {
+        type: DataTypes.STRING,
+    },
+    firstWord: {
+        type: DataTypes.STRING,
+    },
+
+});
+module.exports = Location;
