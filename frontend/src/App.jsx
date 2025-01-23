b5313df839699ccaf8a44cc868e3ae3665ba27e8
Anna <annasaltveit@gmail.com>
2025-01-23 09:08:15 -0800

    frontend-map tooltips working, deleted PlotFigure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import * as Plot from "@observablehq/plot";
 import './App.css';
 import mapData from "./data/custom.geo.json";
-import PlotFigure from "./PlotFigure.jsx";
 
 function App() {
+  const mapRef = useRef();
   const [locations, setLocations] = useState([]);
+
+  let symbols = ["asterisk", "circle", "square", "cross", "triangle"];
+  let types = ["manuscript", "amulet", "inscription", "graffito", "dipinto"];
 
   useEffect(() => {
     axios.get('http://localhost:3000/locations').then((data) => {
@@ -15,40 +18,48 @@ function App() {
     })
   }, []);
 
-  let symbols = ["asterisk", "circle", "square", "cross", "triangle"];
-  let types = ["manuscript", "amulet", "inscription", "graffito", "dipinto"];
+  console.log(locations)
+
+  useEffect(() => {
+    if (locations === undefined) return;
+    const chart = Plot.plot({
+      style: {
+        background: "transparent"
+      },
+      projection: {type: "orthographic", inset: -450, rotate: [-10, -35]},
+        marks: [
+          Plot.geo(mapData),
+          Plot.dot(locations, {
+            x: "longitude",
+            y: "latitude",
+            stroke: "type",
+            fill: "type",
+            fillOpacity: 0.3,
+            //symbol and color need to be bound to type
+            r: 7,
+            symbol: "type",
+            
+          }),
+          Plot.tip(locations, Plot.pointer({
+            x: "longitude",
+            y: "latitude",
+            title: (d) => ["Created from: " + d.created_year_start + "-" + d.created_year_end, "Text: " + d.text, "Place: " + d.place, "Location: " + d.location, "Year Discovered: " + d.discovered_year, "Shelfmark: " + d.shelfmark].join("\n\n")
+          })),
+        ],
+        height: 500, // Canvas height
+        width: 700, // Canvas width
+        symbol: {legend: true, domain: types, range: symbols},
+        color: { domain: types, scheme: "turbo"}
+    });
+    mapRef.current.append(chart);
+    return () => chart.remove();
+  }, [locations]);
 
   return (
     <>
       <h1>ROTAS Map</h1>
       <div className="card">
-        <PlotFigure
-          options={{
-            projection: {type: "orthographic", inset: -450, rotate: [-10, -35]},
-            marks: [
-              Plot.geo(mapData),
-              Plot.dot(locations, {
-                x: "longitude", // Provide longitude values
-                y: "latitude", // Provide latitude values
-                stroke: "type",
-                fill: "type",
-                fillOpacity: 0.3,
-                //symbol and color need to be bound to type
-                r: 7,
-                symbol: "type",
-              }),
-              Plot.tip(locations, Plot.pointer({ // Don't see this show up
-                x: "longitude", // Provide longitude values
-                y: "latitude", // Provide latitude values
-                title: (d) => "woo"//["Created from: " + d.created_year_start + "-" + d.created_year_end, "Text: " + d.text, "Place: " + d.place, "Location: " + d.location, "Year Discovered: " + d.discovered_year, "Shelfmark: " + d.shelfmark].join("\n\n")
-              })),
-            ],
-            height: 500, // Canvas height
-            width: 700, // Canvas width
-            symbol: {legend: true, domain: types, range: symbols},
-            color: { domain: types, scheme: "turbo"} // legend: true,
-          }}
-        />
+        <div ref={mapRef}></div>
       </div>
     </>
   )
@@ -60,6 +71,5 @@ export default App;
 index.css:
  color: rgba(255, 255, 255, 0.87);
    background-color: #242424;
-
 
 */
