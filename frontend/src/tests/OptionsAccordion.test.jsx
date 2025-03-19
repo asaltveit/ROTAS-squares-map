195a6c2220d619ffe6c6f62f078d53588acb01ee
Anna <annasaltveit@gmail.com>
2025-03-18 19:12:25 -0700

    frontend-add accordion tests

--- frontend/src/tests/OptionsAccordion.test.jsx
+++ frontend/src/tests/OptionsAccordion.test.jsx
@@ -1,8 +1,7 @@
 import OptionsAccordion from '../components/OptionsAccordion'
 import React from 'react';
-//import react from '@vitejs/plugin-react'
 import { expect, describe, it, } from 'vitest';
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 
 describe('OptionsAccordion', () => {
@@ -16,7 +15,68 @@ describe('OptionsAccordion', () => {
         render(<OptionsAccordion children={options} />);
         const headingElement = screen.getByLabelText('test1');
         expect(headingElement).toBeInTheDocument();
-        const bodyElement = screen.getByLabelText('test2');
+    })
+
+    it('opens', () => {
+        const options = [
+            {
+                header: "test1",
+                body: <div>test2</div>
+            }
+        ]
+        render(<OptionsAccordion children={options} />);
+
+        const headingElement = screen.getByLabelText('test1');
+        expect(headingElement).toBeInTheDocument();
+
+        fireEvent.click(headingElement)
+
+        const bodyElement = screen.getByText('test2');
         expect(bodyElement).toBeInTheDocument();
+    })
+    it('has multiple accordions', () => {
+        const options = [
+            {
+                header: "test1",
+                body: <div>test2</div>
+            },
+            {
+                header: "test3",
+                body: <div>test4</div>
+            },
+            {
+                header: "test5",
+                body: <div>test6</div>
+            }
+        ]
+        render(<OptionsAccordion children={options} />);
+
+        const heading1Element = screen.getByLabelText('test1');
+        expect(heading1Element).toBeInTheDocument();
+
+        fireEvent.click(heading1Element)
+
+        const body1Element = screen.getByText('test2');
+        expect(body1Element).toBeInTheDocument();
+
+        const heading2Element = screen.getByLabelText('test3');
+        expect(heading2Element).toBeInTheDocument();
+
+        fireEvent.click(heading2Element)
+
+        const body2Element = screen.getByText('test4');
+        expect(body2Element).toBeInTheDocument();
+
+        const heading3Element = screen.getByLabelText('test5');
+        expect(heading3Element).toBeInTheDocument();
+
+        fireEvent.click(heading3Element)
+
+        const body3Element = screen.getByText('test6');
+        expect(body3Element).toBeInTheDocument();
+    })
+    it('handles having no children', () => {
+        render(<OptionsAccordion />)
+        // What to do here?
     })
 })
