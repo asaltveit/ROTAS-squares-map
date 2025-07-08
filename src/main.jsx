05e69ee21cd3bc66c06b835ccaea9bd6c1ea1878
Anna <annasaltveit@gmail.com>
2025-07-08 16:14:04 -0700

    add tab icon

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+// ROTAS Map tab icon - https://en.wikipedia.org/wiki/Sator_Square#/media/File:Palindrom_TENET.svg.png
 
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId="379743926732-29ilslou65co35ln58gasmsqjei9mdc1.apps.googleusercontent.com">
